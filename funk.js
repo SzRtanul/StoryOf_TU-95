2da315e3dd5c7a4921e3804e766c60b957526257
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-02-09 12:14:59 +0100

    True Initial

---
+++ funk.js
@@ -0,0 +1,3 @@
+for (let i = 1; i < 16; i++){
+    document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+i+"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
+}
