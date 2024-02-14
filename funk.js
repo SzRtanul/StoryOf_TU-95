16802850f5aaba2e7c1e53387d27ff8390c8c247
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-02-14 15:36:52 +0100

    has

--- funk.js
+++ funk.js
@@ -1,3 +1,6 @@
-for (let i = 1; i < 16; i++){
-    document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+i+"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
+var sh = require('shelljs');
+
+faq = 1
+for (let i = 1; i < 5; i++){
+    document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+ Number(i*faq) +"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
 }
