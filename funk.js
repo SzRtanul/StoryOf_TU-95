f7ea639a6337d2eff85fc5e2fd1cd92876c62f9a
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-02-23 09:25:00 +0100

    add app.js

--- funk.js
+++ funk.js
@@ -1,6 +1,6 @@
-var sh = require('shelljs');
+let sh = require('shelljs');
 
 faq = 1
-for (let i = 1; i < 5; i++){
+for (let i = 1; i < 16; i++){
     document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+ Number(i*faq) +"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
 }
