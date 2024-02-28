d05ee5ae18fd39e4d75cecb24757bb7097fd6770
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-02-28 16:14:06 +0100

    wokring

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import { SZEMELYLISTA } from "./funk.js"
 import { workwithlist } from "./fuggveny.js"
+let ertek = 0;
 
-let faq = 0 * 4;
-for (let i = 1+faq; i < 5+faq; i++){
-    document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+ Number(i) +"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
+updat();
+function leptet(mertek){
+    ertek += mertek;
+    updat();
+}
+
+function updat(){
+    let faq = ertek * 4;
+    for (let i = 1+faq; i < 5+faq; i++){
+        document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+ Number(i) +"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
+    }
 }
 
 console.log(workwithlist(SZEMELYLISTA))
