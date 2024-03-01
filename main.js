ee9904f720f510a5b71a5c5a1c255ba0aa92d265
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-03-01 11:22:52 +0100

    leptet

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 import { SZEMELYLISTA } from "./funk.js"
 import { workwithlist } from "./fuggveny.js"
+
+let kepekszama = 15;
 let ertek = 0;
+let imgforward = document.getElementById("forwardimage")
+let imgbackward = document.getElementById("backwardimage")
+imgforward.addEventListener("click", function() { leptet(1) })
+imgbackward.addEventListener("click", function() { leptet(-1) })
+
 
 updat();
 function leptet(mertek){
-    ertek += mertek;
-    updat();
-}
-
-function updat(){
-    let faq = ertek * 4;
-    for (let i = 1+faq; i < 5+faq; i++){
-        document.getElementById("story").innerHTML += "<div class=\"storydiv\"id=\"k"+ Number(i) +"\"><img class=\"storyimg\" src=\"kepek/k"+i+".png\" alt=\""+i+"\"></div>"
+    if((mertek < 0 && ertek > 0) || (mertek > 0 && ertek < (kepekszama / 4)-1)){
+        ertek += mertek;
+        updat();
     }
 }
 
+function updat(){
+    let story =document.getElementById("story");
+    let faq = ertek * 4;
+    story.innerHTML = "";
+    for (let i = 1+faq; i < 5+faq; i++){
+        if(LinkCheck(`kepek/k${i}.png`)){
+            story.innerHTML += `<div class=\"storydiv\"id=\"k${i}\"><img id=\"ki${i}\" class=\"storyimg\" src=\"kepek/k${i}.png\" alt=\"${i}\"></div>`
+        }
+        else{
+            story.innerHTML += `<div class=\"storydiv\"id=\"k${i}\"><img id=\"ki${i}\" class=\"storyimg\" src=\"kepek/k0.png\" alt=\"${i}\"></div>`
+        }
+    }
+}
+let copyB = document.getElementById("copy");
+copyB.addEventListener("click", function(){ leptet(1) })
+
 console.log(workwithlist(SZEMELYLISTA))
+
+function LinkCheck(url){
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
+}
